15d4a2a3fa40c730ae94dd2981543d1008c2217c
boltpc2 <info@schoolbolt.ru>
2020-03-22 12:37:47 +0500

    first

---
+++ Pee122/main.cpp
@@ -0,0 +1,140 @@
+#include<iostream>
+#include<ctime>
+using namespace std;
+int n (int a)
+{ for (int i;)
+    return ;}
+int main()
+{
+
+//     cout<<sayHi(2);
+//     cout<<" ";
+//     int b
+//     int c;
+//     int h
+//     cin>>h;
+//     cin>>a;
+//     cin>>b;
+//     cin>>c;
+//     cout<<a*b*c*h;
+//     cout<<b+3;
+//     cout<<c+4;
+//     num3 = num3;
+//     num1 = num1;
+//     num2 = num2;
+//     int a=1;
+//     int b=2;
+//     int c=3;
+//     if(a+b<=c)
+//     { cout<<"no";}
+//     else if (a+b>=c)
+//     {cout<<"yes";}
+//     else if (b+c<=a)
+//     {cout<<"no";}
+//     else if (b+c>=a)
+//     {cout<<"yes";}
+//     int year =2020;
+//     if(year=0)
+//     {cout<<"no";}
+//     else if (year%400==0)
+//     {cout<<"yes";}
+//     else if (year%100==0)
+//     {cout<<"no";}
+//     else if (year==4)
+//     {cout<<"yes";}
+//     int n=25;
+//     int a=1;
+//     int b=1;
+//     int c=0;
+//     cout<< a << " ";
+//     cout<< b << " ";
+//     for (  int i=2;i<n;i++)
+//     { if (c=a+b);
+//     a=b;
+//     b=c;
+//     cout<< c <<" ";}
+//     int n = 3;
+//     for (int i=0;i<n;i++)
+//     {for ( int j=0;j<n+i-3;j++)
+//     {cout<<" ";}
+//     for (int k=0;k<n-i;k++)
+//     {cout<<"*";}
+//     cout<<"\n";}
+//     int n=3;
+//     int a;
+//     int s=0;
+//     for (int i=0;i<n;i++)
+//     {cin>>a;}
+//     int s=0;
+//     int N;
+//     cin>> N ;
+//     while (N!=0)
+//     {s+=N;
+//     cin>>N;}
+//     cout<<s;
+//     int n;
+//     int s=0;
+//     for(;n!=0;)
+//     {s+=n%10;
+//     n=n/10;}
+//     cout<<s;
+//     int n=1234567;
+//     while (!(n<10000 or n>999999))
+//     int n;
+//     {cin>>n;}
+//     if (n>99999 and n<1000000){
+//     int s1=n%10+(n/10)%10+(n/100)%10;
+//     int n1=n/1000;
+//     int s2=n1%10+(n1/10)%10+(n1/100)%10;
+//     if (s1==s2)
+//     cout<<"lucky";
+//     else
+//     cout<<"no";}
+//     int n=1234567;
+//     int c=1;
+//     while (n<10000 or n>999999)
+//     srand(ti
+//     int n;
+//     c++
+//     if (c>3)
+//     {break;}
+//     cin>>n;}
+//     if (n>99999 and n<1000000){
+//     int s1=n%10+(n/10)%10+(n/100)%10;
+//     int n1=n/1000;
+//     int s2=n1%10+(n1/10)%10+(n1/100)%10;
+//     if (s1==s2)
+//     cout<<"lucky";
+//     else
+//     cout<<"no";}
+//     int n =10;
+//     for (int i=2;i<=n/2;i++)
+//     {if (n%i!=0)
+//     continue;
+//     else
+//     cout<<i;}
+//     int n=15;
+//     int i=2;
+//     while(n%i!=0)
+//     i++;
+//     cout<<i;
+//     int min=0;
+//     int max=100;
+//     srand(time(0));
+//     int r=rand()%(max-min+1)+min;
+//     cout<< r;
+//     int min=1;
+//     int max=10;
+ //    int z;
+//     srand(time(0));
+//     int r=rand()%(max-min+1)+min;
+//     while (z==r)
+//     cin>>z;
+//     Функции//
+       //code reuse//
+
+
+
+
+        return 0;
+    }
